1d1c14093b342eb2c0e5972f09ff83cb005fcc65
Nabille Helal <nabilleh@outlook.com>
2019-11-18 15:05:56 +0000

    Add account history

--- lib/account.rb
+++ lib/account.rb
@@ -1,18 +1,39 @@
 class Account
 
-  attr_reader :name, :balance
+  attr_reader :name, :balance, :history
 
   def initialize(name)
     @name = name
     @balance = 0
+    @history = []
   end
 
   def credit(amount)
     @balance += amount
+    update_history(type: "credit", amount: amount)
   end
 
   def debit(amount)
     @balance -= amount
+  end
+
+  private
+
+  def update_history(type:, amount:)
+    @history.push({date: time_stamp, type: type, amount: amount, balance: calc_balance(amount, type) })
+  end
+
+  def time_stamp
+    Time.now.strftime("%m/%d/%Y")
+  end
+
+  def calc_balance(amount, type)
+    return amount if @history.length == 0
+    if type == "credit"
+        return @history.last[:balance] + amount
+    elsif type == "credit"
+        return @history.last[:balance] - amount
+    end
   end
 
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,6 +2,8 @@ require './lib/account'
 
 describe Account do
   subject(:nabille) { described_class.new("nabille") }
+  # let(:history) { double(:history, balances: balances, credits: credits, debits: debits ) }
+  # let(:balances) { "14/01/2012 "}
 
   describe "a new bank account can be opened" do
     it 'should have a name' do
@@ -11,12 +13,22 @@ describe Account do
     it "should open with a balance of zero" do
       expect(nabille.balance).to eq(0)
     end
+
+    it "should have a blank transaction history" do
+      expect(nabille.history).to eq([])
+    end
+
   end
 
   describe "a deposit can be made" do
     it "should update the balance with the amount deposited" do
       nabille.credit(1000)
       expect(nabille.balance).to eq(1000)
+    end
+
+    it "should update the account history after a credit with the date, type, amount and balance" do
+      nabille.credit(1000)
+      expect(nabille.history.first).to eq(date: "01/01/2020", type: "credit", amount: 1000, balance: 1000 )
     end
   end
 
@@ -25,6 +37,12 @@ describe Account do
       nabille.credit(1000)
       nabille.debit(1000)
       expect(nabille.balance).to eq(0)
+    end
+
+    it "should update the account history after a debit with the date, type, amount and balance" do
+      nabille.debit(1000)
+      allow(nabille).to receive(:time_stamp).and_return("01/01/2020")
+      expect(nabille.history.first).to eq(date: "01/01/2020", type: "debit", amount: 200, balance: 800 )
     end
   end
 
