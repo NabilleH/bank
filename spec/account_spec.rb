2815dc029ca63b943f3e80f9579e518226b83450
Nabille Helal <nabilleh@outlook.com>
2019-11-18 12:08:59 +0000

    Add deposit to account

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,17 +1,23 @@
 require './lib/account'
 
 describe Account do
+  subject(:nabille) { described_class.new("nabille") }
 
   describe "a new bank account can be opened" do
     it 'should have a name' do
-      nabille = Account.new("nabille")
       expect(nabille.name).to eq("nabille")
     end
 
     it "should open with a balance of zero" do
-      nabille = Account.new("nabille")
       expect(nabille.balance).to eq(0)
     end
   end
 
+  describe "a deposit can be made" do
+    it "should update the balance with the amount deposited" do
+      nabille.deposit(1000)
+      expect(nabille.balance).to eq(1000)
+    end
+  end
+  
 end
